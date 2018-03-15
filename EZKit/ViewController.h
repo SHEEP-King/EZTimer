718a94bdde6df3ab05fef10cdd166b04c0a0d444
Elon <Elonzhang@163.com>
2018-03-15 17:51:19 +0800

    original project

---
+++ EZKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EZKit
+//
+//  Created by macbook pro on 2018/3/15.
+//  Copyright © 2018年 sheep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EZKit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  EZKit
+//
+//  Created by macbook pro on 2018/3/15.
+//  Copyright © 2018年 sheep. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
